52d3092c8b69de30d0c6266099f0c2b61fd599f1
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2021-08-14 00:00:49 +0200

    Update main.py

--- auto-correction/main.py
+++ auto-correction/main.py
@@ -12,8 +12,6 @@ def main():
         with open("input/correction.csv",mode = "r", encoding='utf-8-sig') as correction, open("input/userinput.csv",mode = "r", encoding='utf-8-sig') as userInput:
             correctDict = csv.DictReader(correction)
             userDict = csv.DictReader(userInput)
-            # print(list(correctDict),"     correctdict\n")
-            # print(list(userDict),"    Userdict\n")
             userlist = analyse(correctDict,userDict)
             result(userlist)
     except Exception:
