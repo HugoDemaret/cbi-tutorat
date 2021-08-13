e7ba1d5fa6eee401ea9b57f1b9235203b3100280
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2021-08-14 00:29:11 +0200

    Update main.py

--- auto-correction/main.py
+++ auto-correction/main.py
@@ -4,16 +4,22 @@
 from analyse import analyse
 from result import result
 import csv
+#Constants
+VERSION="1.0.1"
+INPUT_CORRECT="input/correction.csv"
+INPUT_USER="input/userinput.csv"
 ##########################
 #Start of the main script
 ##########################
 def main():
     try:
-        with open("input/correction.csv",mode = "r", encoding='utf-8-sig') as correction, open("input/userinput.csv",mode = "r", encoding='utf-8-sig') as userInput:
+        with open(INPUT_CORRECT,mode = "r", encoding='utf-8-sig') as correction, open(INPUT_USER,mode = "r", encoding='utf-8-sig') as userInput:
+            print("CSV auto-correction script version : ", VERSION)
             correctDict = csv.DictReader(correction)
             userDict = csv.DictReader(userInput)
             userlist = analyse(correctDict,userDict)
             result(userlist)
+            print("Done!")
     except Exception as e:
         print(e)
 ##########################
