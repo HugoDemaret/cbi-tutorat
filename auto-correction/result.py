513fd41c4bc2e3d20c2cbf4abf38acc9973616bf
HugoDemaret <43760004+HugoDemaret@users.noreply.github.com>
2021-08-15 18:05:13 +0200

    Update result.py

--- auto-correction/result.py
+++ auto-correction/result.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
 #Constants:
-OUTPUT="output/results.txt"
+OUTPUT_RES="output/results.txt"
+OUTPUT_STAT="output/statistics.txt"
 
 #Script starts here:
 ##########################
@@ -10,9 +11,20 @@ def result(userlist):
         result: list -> string
     '''
     try:
-        with open(OUTPUT, mode='w', encoding='utf-8-sig') as output:
+        with open(OUTPUT_RES, mode='w', encoding='utf-8-sig') as output:
             for item in userlist:
                 output.write(str(item).translate(str.maketrans({'{': '', '}': '', '\'' : ''})) + "\n")
+    except Exception as e:
+        print(e)
+
+def printstatistics(statlist):
+    '''This function prints the statistics in a file
+        printstatistics: list -> string
+    '''
+    try:
+        with open(OUTPUT_STAT, mode ='w', encoding='utf-8-sig') as statistics:
+            for item in statlist:
+                statistics.write(str(item))
     except Exception as e:
         print(e)
 
